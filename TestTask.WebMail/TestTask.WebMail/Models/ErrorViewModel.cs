891ed058be5072e88f6d8852839ce5c921547730
lexx_solutions <lexxsolutions@gmail.com>
2022-03-07 19:36:02 +0200

    Add initial app

---
+++ TestTask.WebMail/TestTask.WebMail/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TestTask.WebMail.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
